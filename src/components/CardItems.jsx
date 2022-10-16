3225970a2723725f7641796127292fae470106f4
unknown <busimaximiliano@gmail.com>
2022-10-16 19:09:53 -0300

    arreglo del contador y style

---
+++ src/components/CardItems.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function CardItems({ product }) {
+  const [contador, setContador] = useState(1);
+
+  const add = () => {
+    setContador(contador + 1);
+  };
+
+  const sub = () => {
+    if (contador > 1) {
+      setContador(contador - 1);
+    }
+  };
+  const botonSumar = { backgroundColor: "red" };
+  return (
+    <>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/4c773efd-9f0f-4f6b-8d6e-1b164ee54d2d/image.jpg"
+        />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            <ul>
+              <li>id: {product.id} </li>
+              <li>Nombre: {product.product} </li>
+              <li>Precio: {product.precio} </li>
+            </ul>
+            <div></div>
+          </Card.Text>
+          <div className="container d-flex justify-content-center">
+            <Button
+              disabled={contador > 1 ? false : true}
+              onClick={() => sub()}
+              className="mx-2"
+              variant="primary"
+            >
+              -
+            </Button>
+            <div className="mt-2">{contador}</div>
+            <Button
+              onClick={() => add()}
+              className="mx-2"
+              variant="primary"
+              style={botonSumar}
+            >
+              +
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default CardItems;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import CardItems from "./CardItems";
 
-const Main = ({
-  productos,
-  funcionPrueba,
-  productosDos,
-  conferenciaAmericana,
-}) => {
+const Main = ({ productos, productosDos }) => {
   // const { producto } = productos;
   // console.log(productos);
 
@@ -15,16 +11,6 @@ const Main = ({
 
   //Agrego la funcion de suma y resta
   //pero no puedo hacerlo con una funcion y nada mas, porque no estaria conectando el virtual dom (reac) con el dom (js). Por lo tanto tengo que usar el Hook "useState", para que haya un nexo entre los dos. Voy a seguir usando la funcion de sumar y restar, pero necesito del Hook "useState". Con los states ya renderizamos de manera automatica.
-
-  const [contador, setContador] = useState(1);
-
-  const add = () => {
-    setContador = contador + 1;
-  };
-
-  const sub = () => {
-    setContador = contador - 1;
-  };
 
   return (
     <>
@@ -34,46 +20,13 @@ const Main = ({
           <div className="row">
             <div className="col lg={4}">
               {productos.map((producto) => (
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src="https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/4c773efd-9f0f-4f6b-8d6e-1b164ee54d2d/image.jpg"
-                  />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      <ul>
-                        <li>id: {producto.id} </li>
-                        <li>Nombre: {producto.producto} </li>
-                        <li>Precio: {producto.precio} </li>
-                      </ul>
-                      <div>{conferenciaAmericana}</div>
-                    </Card.Text>
-                    <div className="container d-flex justify-content-center">
-                      <Button
-                        onClick={() => sub()}
-                        className="mx-2"
-                        variant="primary"
-                      >
-                        -
-                      </Button>
-                      <div className="mt-2">{contador}</div>
-                      <Button
-                        onClick={() => add()}
-                        className="mx-2"
-                        variant="primary"
-                      >
-                        +
-                      </Button>
-                    </div>
-                  </Card.Body>
-                </Card>
+                <CardItems product={producto} />
               ))}
             </div>{" "}
           </div>
         </div>
         {/* tercero tengo que inyectar el codigo js al componente que quiero */}
-        <div className="container">
+        {/* <div className="container">
           <div className="row">
             {productosDos.map((productosDos) => (
               <Card style={{ width: "18rem" }}>
@@ -92,7 +45,7 @@ const Main = ({
               </Card>
             ))}{" "}
           </div>
-        </div>
+        </div> */}
       </main>
     </>
   );
